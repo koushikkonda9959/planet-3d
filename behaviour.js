7fad07ef0569601fdcd351271c098de6eec7ee86
koushikkonda9959 <62243500+koushikkonda9959@users.noreply.github.com>
2020-03-28 11:08:07 +0530

    Update behaviour.js

--- behaviour.js
+++ behaviour.js
@@ -1,7 +1,6 @@
 var str;
 setTimeout(logoview,4900);
 function logoview(){
-  document.getElementById('logo_corner').style.display = 'block';
   document.getElementById('full_logo').style.display = 'none';
 }
 function background_text_change(str){
